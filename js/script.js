40ecbe7cb40ab8d4e083f28d4c3c7781113a7434
Hasan Alhasan <h.hasan_89@hotmail.com>
2019-10-16 13:18:09 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// ******************************* 
+// Module 4 Solution
+// *******************************
+//
+//
+
+
+// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+
+var speakWord = "Good Bye";
+
+// STEP 8: Rewrite the 'speak' function such that it is attached to the
+// byeSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+
+
+(function () {
+
+    var names = ["Hasan", "Joud", "Jad", "Jina", "Ahmad", "Tarek", "Layla", "Mostafa", "Dima", "Jyana"];
+    
+    // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
+    // Loop over the names array and say either 'Hello' or "Good Bye"
+    // using the 'speak' method or either helloSpeaker's or byeSpeaker's
+    // 'speak' method.
+    // See Lecture 50, part 1
+    for (var i = 0; i < names.length; i++) {
+    
+      // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
+      // Retrieve the first letter of the current name in the loop.
+      // Use the string object's 'charAt' function. Since we are looking for
+      // names that start with either upper case or lower case 'J'/'j', call
+      // string object's 'toLowerCase' method on the result so we can compare
+      // to lower case character 'j' afterwards.
+      // Look up these methods on Mozilla Developer Network web site if needed.
+
+
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+
+
+      // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
+      // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+      // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+      // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+      // name in the loop.
+      if (firstLetter === 'j') {
+          console.log(Hello + names[i])
+          print (Hello + names[i])
+      } else {
+        console.log(Goodbye + names[i])
+        print (Hello + names[i])
+
+    }
+    }
+    
+    });
+    

72086d52105a06aa895060dab631dc155b275981
Hasan Alhasan <h.hasan_89@hotmail.com>
2019-10-16 17:22:31 +0300

    done

---
+++ js/script-hello.js
@@ -0,0 +1,19 @@
+// ******************************* 
+// Module 4 Solution
+// *******************************
+
+
+( function (window) { 
+  
+   var helloSpeaker ={};
+ 
+ var speakWord = "Hello";
+ 
+ 
+ helloSpeaker.speak = function (name) {
+   console.log(speakWord + " " + name);
+ }
+ 
+ window.helloSpeaker=helloSpeaker;
+ })(window);
+    
